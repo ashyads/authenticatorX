044c52915a11bdd0ad6d7d28883d46c2f15bf04c
Ashish Yadav <ashish.yadav@vetic.in>
2023-07-04 19:23:24 +0530

    Send Otp Api modifications

--- authenticatorX_app/views.py
+++ authenticatorX_app/views.py
@@ -1,10 +1,9 @@
 import json
 
 from authenticatorX.responses import SuccessResponse
-from authenticatorX.settings import SNS_CLIENT
 from rest_framework.views import APIView
 from utils.common_utils import json_schema_validate
-from utils.send_sms import otp_sms_message, generate_random_otp
+from utils.send_sms import generate_random_otp, SendSMS
 from utils.send_whatsapp import SendWhatsapp
 
 
@@ -17,7 +16,7 @@ class SendOTP(APIView):
         json_schema_validate(requested_data, send_otp_api_request_schema)
         mobile = requested_data.get("mobile")
         plain_otp = generate_random_otp()
-        SNS_CLIENT.publish(PhoneNumber=f"+91{mobile}", Message=otp_sms_message(plain_otp))
+        SendSMS().send_otp(mobile=mobile, plain_otp=plain_otp)
         requests.get(SendWhatsapp.opt_in(**{"mobile": mobile}))
-        requests.get(SendWhatsapp.send_otp_001(**{"mobile": mobile, "otp": plain_otp}))
+        requests.get(SendWhatsapp.send_otp(**{"mobile": mobile, "otp": plain_otp}))
         return SuccessResponse(message="Otp Sent Successfully")
